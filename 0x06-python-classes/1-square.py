065f851b735afbdaae46b605a09bdfcbfd7d3dab
quintabel23 <chikwendugrace23@gmail.com>
2022-12-20 16:26:32 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
+# 0-square.py by Chikwendu Grace
 """Defines a square """
 
 
-    class Square:
+class Square:
     """Represents a square"""
 
     def __init__(self, size):
-    """Initializing this square class
-    Args: size - represnets the size of the square defined
-    """
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
 
-    self.__size = size
+        self.__size = size
