02045b20f853db0e29c99b927f3cc422c8857ab6
quintabel23 <chikwendugrace23@gmail.com>
2022-12-21 19:20:26 -0800

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,17 +3,16 @@
 """A module that defines a square """
 
 
-
 class Square:
     """A class that represents a square"""
 
     def __init__(self, size=0):
         """Initializing this square class
         Args:
-        size: represnets the size of the square defined
+            size: represnets the size of the square defined
         Raises:
-        TypeError: if size is not integer
-        ValueError: if size is less than zero
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
         """
 
         if not isinstance(size, int):
